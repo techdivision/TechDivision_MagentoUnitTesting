6fdac5df940f1ffbd3e7978a3fd8c44c41570bb2
wagnert <t.wagner@techdivision.com>
2013-05-22 16:05:48 +0200

    Change licence information to OSL 3.0

--- src/app/code/community/TechDivision/MagentoUnitTesting/Helper/Data.php
+++ src/app/code/community/TechDivision/MagentoUnitTesting/Helper/Data.php
@@ -1,33 +1,11 @@
 <?php
 
 /**
- * License: GNU General Public License
+ * NOTICE OF LICENSE
  *
- * Copyright (c) 2011 TechDivision GmbH. All rights reserved.
- * Note: Original work copyright to respective authors
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * @category   TechDivision
- * @package    TechDivision_MagentoUnitTesting
- * @subpackage Helper
- * @copyright  Copyright (c) 1996-2011 TechDivision GmbH (http://www.techdivision.com)
- * @license	   http://www.gnu.org/licenses/gpl.html GPL, version 3
- * @version    ${release.version}
- * @link       http://www.techdivision.com
- * @since      File available since Release 0.1.0
- * @author     TechDivision Core Team <core@techdivision.com>
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
  */
 
 /**
@@ -36,14 +14,14 @@
  * @category   TechDivision
  * @package    TechDivision_MagentoUnitTesting
  * @subpackage Helper
- * @copyright  Copyright (c) 1996-2011 TechDivision GmbH (http://www.techdivision.com)
- * @license	   http://www.gnu.org/licenses/gpl.html GPL, version 3
  * @version	   ${release.version}
  * @since	   Class available since Release 0.1.0
- * @author     TechDivision Core Team <core@techdivision.com>
+ * @copyright  Copyright (c) 2010 <info@techdivision.com> - TechDivision GmbH
+ * @license    http://opensource.org/licenses/osl-3.0.php
+ *             Open Software License (OSL 3.0)
+ * @author     Tim Wagner <tw@techdivision.com>
  */
 class TechDivision_MagentoUnitTesting_Helper_Data
 	extends Mage_Core_Helper_Abstract
 {
-
 }
